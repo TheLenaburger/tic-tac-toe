e8fd96f1fef9da32270195a9de39c0c3779346ec
TheLenaburger <austin.lenaburg@gmail.com>
2021-10-09 13:56:58 -0500

    Project Groundwork
    Adds curl scripts to test api, updates api url, creates a basic html page with user forms, and creates the auth/api, app, events, and ui files for modular development

---
+++ curl-scripts/auth/sign-in.sh
@@ -0,0 +1,14 @@
+# EMAIL=arlin@email.com PASSWORD=sword sh curl-scripts/auth/sign-in.sh
+
+curl "https://tic-tac-toe-api-development.herokuapp.com/sign-in" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+  "credentials": {
+    "email": "'"${EMAIL}"'",
+    "password": "'"${PASSWORD}"'"
+  }
+}'
+
+echo
