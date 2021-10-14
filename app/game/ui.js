058e5eeeb624fee751aaced5528707d2d74f642f
TheLenaburger <austin.lenaburg@gmail.com>
2021-10-14 00:47:10 -0500

    CSS Overhaul
    Adds multiple flex containers to html and utilizes more bootstrap functions for more responsive design, condenses CSS for the game-board utilizing calc() functions for responsive square shape, and modifies all ui event-handlers for updated targets.

--- app/game/ui.js
+++ app/game/ui.js
@@ -1,8 +1,13 @@
 // contains the ui events for game-related content
 
-const startNewGame = () => {
+const store = require('../store')
+
+const startNewGame = (responseData) => {
+  // Store the game data for us to use in future API calls
+  store.game = responseData.game
+
   $('#play-mat').show(500)
-  $('#gameplay-feedback').text("Make your move! Show 'em what you got!")
+  $('#gameplay-feedback').text("Make your move! Show 'em what you got! X is up first!")
 }
 
 module.exports = {
